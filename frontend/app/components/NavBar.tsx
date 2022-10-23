2a0a9d5e748cac347b58ff73c41cf6954c552e12
testA113 <alex.harris@portainer.io>
2022-10-23 18:32:52 +1300

    setup daisyui theme

--- frontend/app/components/NavBar.tsx
+++ frontend/app/components/NavBar.tsx
@@ -1,8 +1,10 @@
 export const NavBar = () => {
   return (
-    <div className="flex justify-between">
-      <div>left</div>
-      <div>right</div>
+    <div className="px-5vw py-9 lg:py-12">
+      <nav className="text-primary mx-auto flex max-w-8xl items-center justify-between">
+        <div>left</div>
+        <div>right</div>
+      </nav>
     </div>
   );
 };
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./app/**/*.{tsx,ts,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "5vw": "5vw", // pull featured sections and navbar in the margin
+        "8vw": "8vw", // positions hero img inside the margin
+        "10vw": "10vw", // page margin
+      },
+    },
   },
   plugins: [require("daisyui")],
-}
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    base: true,
+    utils: true,
+    prefix: "",
+    themes: [
+      {
+        alchemist: {
+          primary: "#D5BA5A",
+          secondary: "#263F40",
+          accent: "#11596F",
+          neutral: "#78716c",
+          "base-100": "#1c1917",
+          info: "#8CCAC1",
+          success: "#9CB686",
+          warning: "#fb923c",
+          error: "#FC9783",
+        },
+      },
+    ],
+  },
+};
