1a2e95e3ec215a7bdd6d60a42ad5857f4d013380
testA113 <83188384+testA113@users.noreply.github.com>
2022-12-12 08:05:48 +1300

    move contact form to component

--- frontend/app/components/sections/ContactForm.tsx
+++ frontend/app/components/sections/ContactForm.tsx
@@ -1,3 +1,4 @@
+import { Form } from "@remix-run/react";
 import clsx from "clsx";
 
 import type { ContactFormValues } from "./types";
@@ -7,17 +8,58 @@ type Props = {
 };
 
 export function ContactForm({ sectionData }: Props) {
-  // console.log(sectionData);
+  console.log(sectionData);
   return (
     <section
       className={clsx(
         "bg-base-100 flex flex-col",
-        "px-10vw w-full !max-w-full items-center py-24"
+        "px-10vw w-full !max-w-full items-center py-12"
       )}
     >
       <div className="prose md:prose-lg lg:prose-xl mb-12">
-        <h1>{sectionData.title}</h1>
+        <h1>Contact Us</h1>
       </div>
+      <Form method="post">
+        <div className="flex flex-col gap-y-4">
+          <p>
+            <label className="flex flex-col">
+              Hi, my name is:
+              <input
+                name="name"
+                type="text"
+                max={100}
+                placeholder="Slim Shady"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </p>
+          <p>
+            <label className="flex flex-col">
+              You can reach me at:
+              <input
+                name="email"
+                type="email"
+                max={150}
+                placeholder="Your email address"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </p>
+          <p>
+            <label className="flex flex-col">
+              And here's some info about my event:
+              <textarea
+                name="description"
+                className="textarea textarea-bordered"
+                placeholder="Event description, guest numbers, location, favourite drinks.."
+              ></textarea>
+            </label>
+          </p>
+          <button className="btn btn-primary btn-md" type="submit">
+            Submit
+          </button>
+        </div>
+      </Form>
     </section>
   );
 }
