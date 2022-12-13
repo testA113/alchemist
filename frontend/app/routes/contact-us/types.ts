d591df4b65d9581a31f0b1e008f8cf79d0ed94c1
testa113 <testa113>
2022-12-14 01:31:16 +1300

    return formatted error messages to cntact form

--- frontend/app/routes/contact-us/types.ts
+++ frontend/app/routes/contact-us/types.ts
@@ -1,7 +1,23 @@
+import type { StrapiError } from "../utils.server";
+
 export type ContactMessage = {
-  data: {
-    name: string;
-    email: string;
-    description: string;
-  };
+  name: string;
+  email: string;
+  description: string;
 };
+
+export type ContactMessagePayload = {
+  data: ContactMessage;
+};
+
+export type StrapiContactMessageResponse = {
+  data: ContactMessage | null;
+  error: StrapiError<keyof ContactMessage> | null;
+};
+
+export type ContactMessageActionData =
+  | {
+      error: Record<keyof ContactMessage, string>;
+      values: ContactMessage;
+    }
+  | undefined;
