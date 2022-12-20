44f4e11f16ddc8793300805ce1712e2be2426d5b
testa113 <testa113>
2022-12-20 23:36:07 +1300

    add recaptchav3 validation

--- frontend/app/routes/contact-us/contact-us.server.ts
+++ frontend/app/routes/contact-us/contact-us.server.ts
@@ -32,3 +32,61 @@ export const getContactPage = async () => {
     }
   );
 };
+
+// create assesment is modified from https://cloud.google.com/recaptcha-enterprise/docs/create-assessment#rest-api with node js error handling
+export const createAssessment = async (token: string) => {
+  // build the assesment request - hardcoded is fine for now
+  const expectedAction = "contact";
+  const projectID = "alchemist-1671430214352";
+  const siteKey = "6LeL9o0jAAAAANddsjQmD7-Xls1boV7i1DumFVfE";
+  const apiKey = process.env.CAPTCHA_API_KEY;
+  const requestBody = {
+    event: {
+      token,
+      siteKey,
+      expectedAction,
+    },
+  };
+
+  // fetch the assessment
+  const response = await fetch(
+    `https://recaptchaenterprise.googleapis.com/v1/projects/${projectID}/assessments?key=${apiKey}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    }
+  );
+  const responseBody = await response.json();
+
+  // Check if the token is valid.
+  if (!responseBody.tokenProperties?.valid) {
+    console.log(
+      "The CreateAssessment call failed because the token was: " +
+        responseBody.tokenProperties?.invalidReason
+    );
+
+    return false;
+  }
+
+  // Check if the expected action was executed.
+  if (responseBody.event.expectedAction === expectedAction) {
+    // Get the risk score and the reason(s).
+    // For more information on interpreting the assessment,
+    // see: https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+    console.log("The reCAPTCHA score is: " + responseBody.riskAnalysis?.score);
+
+    responseBody.riskAnalysis?.reasons?.forEach((reason: string) => {
+      console.log(reason);
+    });
+    return responseBody.riskAnalysis?.score || false;
+  }
+
+  console.log(
+    "The action attribute in your reCAPTCHA tag " +
+      "does not match the action you are expecting to score"
+  );
+  return false;
+};
