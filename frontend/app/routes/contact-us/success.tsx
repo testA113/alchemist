3eca59ec5d22e652fe370fb0b8ff195463e684df
testa113 <testa113>
2023-01-03 20:28:14 +1300

    update success message

--- frontend/app/routes/contact-us/success.tsx
+++ frontend/app/routes/contact-us/success.tsx
@@ -1,4 +1,3 @@
-
 export default function ContactUsSuccess() {
   return (
     <div className="min-h-minpage pt-32">
@@ -9,9 +8,27 @@ export default function ContactUsSuccess() {
         <div className="flex flex-col gap-y-2">
           <div className="text-center">
             <p className="mb-4">
-              We'll be in touch with you shortly. In the meantime check out our
-              work, or what we've been up to on our socials below.
+              Thanks for enquiring about our portable premium bar service, we
+              are excited to learn more about your function!
             </p>
+            <p className="mb-4">
+              Our functions coordinator, Jazmin Campbell, will be in touch
+              shortly to understand your requirements - you can contact her on{" "}
+              <a
+                aria-label="Phone"
+                href="tel: +64226834889"
+                className="underline"
+              >
+                022 683 4889
+              </a>
+              !
+            </p>
+            <p className="mb-4">
+              In the meantime check out our work, or what we've been up to on
+              our socials below.
+            </p>
+            <p className="mb-4">Speak to you soon!</p>
+            <p className="mb-4">Best Wishes, Alchemist Mixology</p>
           </div>
         </div>
       </section>
