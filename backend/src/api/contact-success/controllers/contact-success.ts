671996c424f99832f85e6f954a11b29593ef61e5
testa113 <testa113>
2023-01-09 23:54:35 +1300

    contact success for analytics

---
+++ backend/src/api/contact-success/controllers/contact-success.ts
@@ -0,0 +1,7 @@
+/**
+ * contact-success controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::contact-success.contact-success');
---
+++ backend/src/api/contact-success/routes/contact-success.ts
@@ -0,0 +1,7 @@
+/**
+ * contact-success router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::contact-success.contact-success');
