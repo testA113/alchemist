30ea96e86cdb44f6a454f8a9d081887af9cdd80b
testA113 <83188384+testA113@users.noreply.github.com>
2023-01-09 23:14:06 +1300

    g4 anayltics with partytown

--- backend/dist/config/plugins.js
+++ backend/dist/config/plugins.js
@@ -2,7 +2,7 @@ module.exports = {
     "upload-plugin-cache": {
         enabled: true,
         config: {
-            maxAge: 3600 * 24 * 30, // 30 day cache for media files (when the file path is the same)
+            maxAge: 3600 * 24 * 7,
         },
     }
 };
