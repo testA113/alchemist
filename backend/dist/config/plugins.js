46b03c9a2c28346e8994b8778c3c0e9742c0df65
testA113 <83188384+testA113@users.noreply.github.com>
2023-01-18 00:12:25 +1300

    use cloudfront

--- backend/dist/config/plugins.js
+++ backend/dist/config/plugins.js
@@ -9,13 +9,15 @@ exports.default = ({ env }) => ({
     },
     upload: {
         config: {
-            provider: 'aws-s3',
+            provider: 'strapi-provider-upload-aws-s3-advanced',
             providerOptions: {
                 accessKeyId: env('AWS_ACCESS_KEY_ID'),
                 secretAccessKey: env('AWS_ACCESS_SECRET'),
                 region: 'ap-southeast-2',
+                baseUrl: env('CDN_BASE_URL'),
                 params: {
                     Bucket: env('AWS_BUCKET'),
+                    acl: 'public-read',
                 },
             },
             actionOptions: {
