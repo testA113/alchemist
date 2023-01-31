b138d35e5c5089871de89428b04411337403435b
testa113 <testa113>
2023-01-31 14:52:26 +1300

    add structured data

--- frontend/app/routes/about-us.tsx
+++ frontend/app/routes/about-us.tsx
@@ -1,9 +1,10 @@
-import { json, type MetaFunction } from "@remix-run/node";
+import { json, type SerializeFrom, type MetaFunction } from "@remix-run/node";
 import { getPage } from "./index.server";
 import type { GetAttributesValues } from "@strapi/strapi";
 import { useLoaderData } from "@remix-run/react";
 import { Section } from "~/components/sections";
 import { getStrapiSeo } from "~/utils/seo";
+import { type StructuredDataFunction } from "remix-utils";
 
 export async function loader() {
   const aboutUsResponse = await getPage("about-us", {
@@ -34,6 +35,20 @@ export const meta: MetaFunction = ({ data }) => {
   };
   return getStrapiSeo(seo);
 };
+
+let structuredData: StructuredDataFunction<
+  SerializeFrom<typeof loader>,
+  any
+> = ({ data }) => {
+  const {
+    aboutUsData: { seo },
+  } = data as {
+    aboutUsData: GetAttributesValues<"api::about-us.about-us">;
+  };
+
+  return seo.structuredData;
+};
+export let handle = { structuredData };
 
 export default function AboutUs() {
   const {
